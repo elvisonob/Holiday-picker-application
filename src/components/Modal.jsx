15b9a9aa16991b35a3cac20ce2b551e044b5e8c2
elvisonob <elvisonob@yahoo.com>
2025-01-10 06:53:34 +0000

    cleanUp function

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,20 @@
-import { forwardRef, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 const Modal = function Modal({ open, children }) {
   const dialog = useRef();
 
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
+
   return createPortal(
-    <dialog className="modal" ref={dialog} open={open}>
-      {children}
+    <dialog className="modal" ref={dialog}>
+      {open ? children : null}
     </dialog>,
     document.getElementById('modal')
   );
