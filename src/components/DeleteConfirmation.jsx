fcae181314251e49d7a2ff492d6da31fd9ec9a69
elvisonob <elvisonob@yahoo.com>
2025-01-15 04:51:55 +0000

    cleaning the effect

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  console.log('TIMER SET');
-  setTimeout(() => {
-    onConfirm();
-    console.log('TIMER DONE');
-  }, 3000);
+  useEffect(() => {
+    console.log('TIMER SET');
+    const timer = setTimeout(() => {
+      onConfirm();
+      console.log('TIMER DONE');
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
